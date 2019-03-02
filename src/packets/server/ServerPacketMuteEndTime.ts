a177611085535a379691285c54b1828c63a50f98
Swan <me@swan.moe>
2019-03-02 12:39:12 -0500

    Add mute endtime packet

---
+++ src/packets/server/ServerPacketMuteEndTime.ts
@@ -0,0 +1,23 @@
+import Packet from "../Packet";
+import PacketId from "../PacketId";
+import { JsonObject, JsonProperty } from "json2typescript";
+import User from "../../sessions/User";
+
+@JsonObject("ServerPacketMuteEndTime")
+export default class ServerPacketMuteEndTime extends Packet {
+
+    public Id: PacketId = PacketId.ServerMuteEndTimePacket;
+
+    @JsonProperty("u")
+    public UserId: number;
+
+    @JsonProperty("t")
+    public MuteEndTime: number;
+
+    constructor(user: User, time: number) {
+        super();
+
+        this.UserId = user.Id;
+        this.MuteEndTime = time;
+    }
+}
