79664ca428eb1c446e4e85237b581990b4c23ad3
Swan <me@swan.moe>
2022-01-25 11:06:42 -0500

    Remove log

--- src/handlers/PongHandler.ts
+++ src/handlers/PongHandler.ts
@@ -4,7 +4,6 @@ import ClientPacketPong from "../packets/client/ClientPacketPong";
 import User from "../sessions/User";
 import * as Discord from "discord.js";
 import DiscordWebhookHelper from "../discord/DiscordWebhookHelper";
-import { date } from "azure-storage";
 const crypto = require('crypto');
 
 export default class PongHandler {
@@ -45,8 +44,6 @@ export default class PongHandler {
                 return;
             }
 
-            console.log(JSON.stringify(detectedProcesses) == JSON.stringify(user.LastDetectedProcesses))
-            
             // Running processes are the same as the last time it was provided.
             if (JSON.stringify(detectedProcesses) == JSON.stringify(user.LastDetectedProcesses))
                 return;
