c37532f7bd6845e06c8b1d30e6570ebe03998623
Swan <me@swan.moe>
2022-02-17 11:49:29 -0500

    Allow Swan usergroup to bypass multi-specate passwords

--- src/sessions/User.ts
+++ src/sessions/User.ts
@@ -570,7 +570,7 @@ export default class User implements IPacketWritable, IStringifyable {
         if (!game)
             return this.SendJoinGameFailurePacket(JoinGameFailureReason.MatchNoExists);
 
-        if (game.HasPassword && game.Password != password && !game.PlayersInvited.includes(this))
+        if (game.HasPassword && game.Password != password && !game.PlayersInvited.includes(this) && !this.IsSwan())
             return this.SendJoinGameFailurePacket(JoinGameFailureReason.Password);
 
         // Remove the player from the lobby if they're currently in it.
